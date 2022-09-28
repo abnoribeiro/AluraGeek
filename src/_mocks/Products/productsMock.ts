22eb6a42586d92df2e0ac1fdeb859cb12967e319
Abno Ribeiro <abno.ribeiro@wisereducacao.com>
2022-09-28 00:07:13 -0300

    chore: adjust mocking data structure

---
+++ src/_mocks/Products/productsMock.ts
@@ -0,0 +1,152 @@
+export interface Products {
+  category: string;
+  products: {
+    title: string;
+    price: number;
+    image: string;
+  }[];
+}
+
+export const productsMocks: Products[] = [
+  {
+    category: 'Star Wars',
+    products: [
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+    ],
+  },
+  {
+    category: 'Consoles',
+    products: [
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+    ],
+  },
+  {
+    category: 'Diversos',
+    products: [
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+      {
+        title: 'Produto XYZ',
+        price: 60.0,
+        image:
+          'https://img.elo7.com.br/product/original/36C432A/chaveiro-emborrachado-star-wars-chaveiros-emborrachados.jpg',
+      },
+    ],
+  },
+];
